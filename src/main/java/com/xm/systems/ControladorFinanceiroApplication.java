75f10acf1ecdec1984ccd798984056ffa595956b
rodrigoaa1988 <rodrigoanjosalmeida@gmail.com>
2023-10-16 12:21:54 -0300

    primeiro commit

---
+++ src/main/java/com/xm/systems/ControladorFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.xm.systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xm/systems/ControladorFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xm.systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControladorFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
